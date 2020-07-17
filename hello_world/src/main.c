6ac125a34477e91d839717c3cb2fa0dd016e73c0
Marcus Comstedt <marcus@mc.pp.se>
2020-07-17 22:52:09 +0200

    Change clock and HAL setup to match what the original firmware is doing

--- hello_world/src/main.c
+++ hello_world/src/main.c
@@ -16,6 +16,23 @@ void _Error_Handler(char *file, int line)
 	/* USER CODE END Error_Handler_Debug */
 }
 
+
+/**
+  * @brief  Initialize the MSP.
+  * @retval None
+  */
+void HAL_MspInit(void)
+{
+	HAL_NVIC_SetPriorityGrouping(NVIC_PRIORITYGROUP_4);
+	HAL_NVIC_SetPriority(MemoryManagement_IRQn, 0, 0);
+	HAL_NVIC_SetPriority(BusFault_IRQn, 0, 0);
+	HAL_NVIC_SetPriority(UsageFault_IRQn, 0, 0);
+	HAL_NVIC_SetPriority(SVCall_IRQn, 0, 0);
+	HAL_NVIC_SetPriority(DebugMonitor_IRQn, 0, 0);
+	HAL_NVIC_SetPriority(PendSV_IRQn, 0, 0);
+	HAL_NVIC_SetPriority(SysTick_IRQn, 0, 0);
+}
+
 /** System Clock Configuration
 */
 void SystemClock_Config(void)
@@ -23,21 +40,21 @@ void SystemClock_Config(void)
 
 	RCC_OscInitTypeDef RCC_OscInitStruct;
 	RCC_ClkInitTypeDef RCC_ClkInitStruct;
+	RCC_PeriphCLKInitTypeDef PeriphClkInit;
 
 	/**Configure the main internal regulator output voltage 
     */
 	__HAL_RCC_PWR_CLK_ENABLE();
 
-	__HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE1);
+	__HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE2);
 
 	/* Enable HSE Oscillator and activate PLL with HSE as source */
-	RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
-	RCC_OscInitStruct.HSIState = RCC_HSI_ON;
-	RCC_OscInitStruct.HSICalibrationValue = 16;
-	RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSI;
-	RCC_OscInitStruct.PLL.PLLM = (HSI_VALUE / 1000000u);
+	RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
+	RCC_OscInitStruct.HSEState = RCC_HSE_ON;
+	RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
+	RCC_OscInitStruct.PLL.PLLM = (HSE_VALUE / 2000000u);
 
-	RCC_OscInitStruct.PLL.PLLN = 336;
+	RCC_OscInitStruct.PLL.PLLN = 168;
 
 	RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV4;
 	RCC_OscInitStruct.PLL.PLLQ = 7; /* To make USB work. */
@@ -51,8 +68,19 @@ void SystemClock_Config(void)
 	RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
 	RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
 	RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV2;
-	RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV2;
+	RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
 	HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_2);
+
+	/**Configure clock outputs */	
+	PeriphClkInit.PeriphClockSelection = RCC_PERIPHCLK_I2S;
+	PeriphClkInit.PLLI2S.PLLI2SN = 100;
+	PeriphClkInit.PLLI2S.PLLI2SR = 4;
+	HAL_RCCEx_PeriphCLKConfig(&PeriphClkInit);
+	HAL_RCC_MCOConfig(RCC_MCO1, RCC_MCO1SOURCE_PLLCLK, RCC_MCODIV_2);
+	HAL_RCC_MCOConfig(RCC_MCO2, RCC_MCO2SOURCE_PLLI2SCLK, RCC_MCODIV_5);
+
+	/**Enable Clock Security System */
+	HAL_RCC_EnableCSS();
 
 	/**Configure the Systick interrupt time 
     */
@@ -63,7 +91,7 @@ void SystemClock_Config(void)
 	HAL_SYSTICK_CLKSourceConfig(SYSTICK_CLKSOURCE_HCLK);
 
 	/* SysTick_IRQn interrupt configuration */
-	HAL_NVIC_SetPriority(SysTick_IRQn, 15, 0);
+	HAL_NVIC_SetPriority(SysTick_IRQn, 0, 0);
 }
 
 int main()
